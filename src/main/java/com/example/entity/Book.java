216fe083bba1930f75f6f34975c3930aef622c3b
Fab11 <fab.stazi11@gmail.com>
2023-08-03 21:53:08 +0200

    created entity, modified application.properties

---
+++ src/main/java/com/example/entity/Book.java
@@ -0,0 +1,71 @@
+package com.example.entity;
+
+
+import com.example.enums.StatusEnum;
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+    @Column
+    private String author;
+    @Column
+    private String isbn;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private StatusEnum status;
+
+
+    public Book() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
